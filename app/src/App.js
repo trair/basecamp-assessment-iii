09a693701983cc70626a1425a226f76343e923e8
Jonathan McDonald <Jonathan@Jonathan.local>
2018-10-11 13:02:59 -0600

    Initial commit

---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div class="item">
+        <h1>Junius R. Ward House</h1>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Erwin.JPG/500px-Erwin.JPG" alt="Junius R Ward house" class="rounded"></img>
+        <p class="caption">The Junius R. Ward House in Erwin, Mississippi is also known as the Erwin House.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
